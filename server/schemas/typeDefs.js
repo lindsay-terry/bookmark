73693810ddd2f85a3b3922fee64165009aa0cbba
Lindsay Terry <lindsaytee66@gmail.com>
2024-09-08 14:04:49 -0600

    Defines type defintions for graphql queries and mutations

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const typeDefs = `
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [Book]!
+    }
+
+    type Book {
+        _id: ID!
+        authors: [String]
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        getSingleUser(_id: ID, username: String): User
+    }
+
+    type Mutation {
+        createUser(username: String!, email: String!, password: String!): Auth
+        login(username: String!, email: String!, password: String!): Auth
+        saveBook(title: String!, _id: ID!): User
+        deleteBook(_id: ID!): User
+    }
+
+    }
+`;
+
+module.exports = { typeDefs }
